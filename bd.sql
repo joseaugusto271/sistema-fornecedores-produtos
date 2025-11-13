33a03ca2a1efcb9e344cd6aae4f58e4636f0d91d
José Augusto Gonçalves da Silva <jose.a.silva271@aluno.senai.br>
2025-11-13 09:45:50 -0300

    Criação dos arquivos do projeto
    Arquivos para funcionamento do sistema de login, cadastro de fornecedores e produtos e listagem de produtos.

---
+++ bd.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE IF NOT EXISTS sistema;
+
+USE sistema;
+
+-- Expressão SQL para criar a tabela de usuarios
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(50) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+-- Expressão SQL para criar a tabela de fornecedores
+CREATE TABLE IF NOT EXISTS fornecedores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    telefone VARCHAR(20)
+);
+
+-- Expressão SQL para criar a tabela de produtos relacionada via FK com a tabela de fornecedores
+CREATE TABLE IF NOT EXISTS produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fornecedor_id INT,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10, 2),
+    tipo ENUM ('Manutenção', 'Alimentação', 'Loja', 'Segurança', 'Serviço') NOT NULL,
+    validade DATE NOT NULL,
+    FOREIGN KEY (fornecedor_id) REFERENCES fornecedores(id)
+);
+
+-- Expressão SQL para cadastrar um usuário
+INSERT INTO usuarios (usuario, senha) VALUES ('Ignacio', MD5('123'));
+INSERT INTO usuarios (usuario, senha) VALUES ('Valmir', MD5('123'));
+INSERT INTO usuarios (usuario, senha) VALUES ('Nadja', MD5('123'));
+INSERT INTO usuarios (usuario, senha) VALUES ('Denis', MD5('123'));
